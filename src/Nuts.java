902867c0b298ed3c8ac49dbf43500c8be9ff30d4
vdonchev <ya.5164646777752015@yandex.ru>
2015-10-24 17:53:29 +0300

    solved

--- src/Nuts.java
+++ src/Nuts.java
@@ -3,21 +3,28 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
-public class OfficeStuff {
+public class Nuts {
 
     static TreeMap<String, LinkedHashMap<String, Integer>> orders;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int numOfOrders = scanner.nextInt();
         scanner.nextLine();
+
         orders = new TreeMap<>();
+
         for (int i = 0; i < numOfOrders; i++) {
             String inputOrder = scanner.nextLine();
-            String[] tokens = inputOrder.substring(1).substring(0, inputOrder.length() - 2).split(" - ");
+
+            String[] tokens = inputOrder
+                    .substring(0, inputOrder.length() - 2)
+                    .split(" ");
+
             String companyName = tokens[0];
-            String productName = tokens[2];
-            int productAmount = Integer.parseInt(tokens[1]);
+            String productName = tokens[1];
+            int productAmount = Integer.parseInt(tokens[2]);
 
             if (!orders.containsKey(companyName)) {
                 orders.put(companyName, new LinkedHashMap<>());
@@ -28,18 +35,20 @@ public class OfficeStuff {
             }
 
             int currentProductAmount = orders.get(companyName).get(productName);
+
             orders.get(companyName).put(productName, currentProductAmount + productAmount);
         }
 
         for (String company : orders.keySet()) {
             System.out.printf("%s: ", company);
+
             boolean firstInList = true;
             for (Map.Entry<String, Integer> product : orders.get(company).entrySet()) {
                 if (firstInList) {
-                    System.out.printf("%s-%d", product.getKey(), product.getValue());
+                    System.out.printf("%s-%dkg", product.getKey(), product.getValue());
                     firstInList = false;
                 } else {
-                    System.out.printf(", %s-%d", product.getKey(), product.getValue());
+                    System.out.printf(", %s-%dkg", product.getKey(), product.getValue());
                 }
             }
 
